3448d2dd21627d02c00369ceeb45372fbd4a89a0
Kevin_Le <Kevle12385@ymail.com>
2024-03-03 20:27:07 -0800

    commit

--- index.js
+++ index.js
@@ -2,20 +2,43 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-
+const { MongoClient } = require('mongodb');
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const uri ="mongodb+srv://kevle12385:3916Cats@cluster0.ri2fueh.mongodb.net/";
 
-const dbURI = "mongodb+srv://kevle12385:3916Cats@cluster0.ri2fueh.mongodb.net/"
+const client = new MongoClient(uri);
+async function main(){
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected…'))
-  .catch(err => console.error(err));
-
+  const uri = "mongodb+srv://kevle12385:3916Cats@cluster0.ri2fueh.mongodb.net/";
 
 
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 
 
